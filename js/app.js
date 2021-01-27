3c4724ccab134f74d24a0cad6184351f7eb824c7
plaurion1989 <plaurion@protonmail.com>
2021-01-26 22:24:18 -0800

    added initial files and framework

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('Hello World');
+
+let hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
+let seattle = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+};
